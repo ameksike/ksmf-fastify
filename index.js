170bde9b4bbe1a948570df47ff20c104f2efafb6
amembrides <antonio.membrides@sita.aero>
2024-03-10 20:41:53 +0100

    update the interface

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const FastifyServer = require('./src/FastifyServer');
-module.exports = new (class extends FastifyServer {
-    cls = {
+
+class FastifyPlugin extends FastifyServer {
+    static cls = {
         Server: FastifyServer,
         Response: require('./src/FastifyResponse'),
         Request: require('./src/FastifyRequest'),
         middleware: {}
     };
-});
+};
+
+module.exports = FastifyPlugin;
